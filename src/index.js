ee012cb1c3468c01b51bb2a1a562b5dfa346b30e
Rob4ert <rob.ryb4@gmail.com>
2022-06-15 16:23:06 +0100

    refactor

--- src/index.js
+++ src/index.js
@@ -3,11 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Demo from "./demo.js"
 
 ReactDOM.render(
   <React.StrictMode>
-    <Demo />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
